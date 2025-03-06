65ecca6db49f0f69cb0fd14e9c2804fbeaa2b01f
Nadyaaurora <nadyaaurora575@gmail.com>
2025-03-06 22:47:02 +0700

    modifikasi pertanyaan 3

--- SMT. 2/ALSD/WEEK 3/source code/MatakuliahDemo19.java
+++ SMT. 2/ALSD/WEEK 3/source code/MatakuliahDemo19.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
+
 public class MatakuliahDemo19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Matakuliah19[] arrayOfMatakuliah = new Matakuliah19[3];
 
-        for (int i = 0; i < arrayOfMatakuliah.length; i++) {
-            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMatkul = sc.nextInt();
+        sc.nextLine();
+
+        Matakuliah19[] arrayOfMatakuliah = new Matakuliah19[jumlahMatkul];
+
+        for (int i = 0; i < jumlahMatkul; i++) {
+            System.out.println("\nMasukkan Data Matakuliah ke-" + (i + 1));
             System.out.print("Kode       : ");
             String kode = sc.nextLine();
             System.out.print("Nama       : ");
@@ -14,12 +20,12 @@ public class MatakuliahDemo19 {
             int sks = sc.nextInt();
             System.out.print("Jumlah Jam : ");
             int jumlahJam = sc.nextInt();
-            sc.nextLine(); 
-            System.out.println("------------------------------------");
+            sc.nextLine();
+
             arrayOfMatakuliah[i] = new Matakuliah19(kode, nama, sks, jumlahJam);
         }
 
-        System.out.println("\n===== Data Matakuliah =====");
+        System.out.println("\n===== Data Mata Kuliah =====");
         for (Matakuliah19 mk : arrayOfMatakuliah) {
             mk.cetakInfo();
         }
