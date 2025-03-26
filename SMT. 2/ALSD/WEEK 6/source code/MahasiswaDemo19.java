64da60595362a6c1356b948a1de5050c2c65c1b7
Nadyaaurora <nadyaaurora575@gmail.com>
2025-03-26 22:47:36 +0700

    feat : percobaan 2 ipk - selection sort

--- SMT. 2/ALSD/WEEK 6/source code/MahasiswaDemo19.java
+++ SMT. 2/ALSD/WEEK 6/source code/MahasiswaDemo19.java
@@ -1,11 +1,17 @@
 public class MahasiswaDemo19 {
     public static void main(String[] args) {
         MahasiswaBerprestasi19 list = new MahasiswaBerprestasi19();
-        Mahasiswa19 m1 = new Mahasiswa19("123", "Zidan", "2A", 3.2);
-        Mahasiswa19 m2 = new Mahasiswa19("124", "Ayu", "2A", 3.5);
-        Mahasiswa19 m3 = new Mahasiswa19("125", "Sofi", "2A", 3.1);
-        Mahasiswa19 m4 = new Mahasiswa19("126", "Sita", "2A", 3.9);
-        Mahasiswa19 m5 = new Mahasiswa19("127", "Miki", "2A", 3.7);
+        // Mahasiswa19 m1 = new Mahasiswa19("123", "Zidan", "2A", 3.2);
+        // Mahasiswa19 m2 = new Mahasiswa19("124", "Ayu", "2A", 3.5);
+        // Mahasiswa19 m3 = new Mahasiswa19("125", "Sofi", "2A", 3.1);
+        // Mahasiswa19 m4 = new Mahasiswa19("126", "Sita", "2A", 3.9);
+        // Mahasiswa19 m5 = new Mahasiswa19("127", "Miki", "2A", 3.7);
+
+        Mahasiswa19 m1 = new Mahasiswa19("123", "Ali", "2B", 3.9);
+        Mahasiswa19 m2 = new Mahasiswa19("124", "Ila", "2B", 3.1);
+        Mahasiswa19 m3 = new Mahasiswa19("125", "Agus", "2B", 3.6);
+        Mahasiswa19 m4 = new Mahasiswa19("126", "Tika", "2B", 3.3);
+        Mahasiswa19 m5 = new Mahasiswa19("127", "Udin", "2B", 3.2);
 
         list.tambah(m1);
         list.tambah(m2);
@@ -16,8 +22,12 @@ public class MahasiswaDemo19 {
         System.out.println("Data mahasiswa sebelum sorting : ");
         list.tampil();
 
-        System.out.println("Data mahasiswa setelah sorting berdasarkan IPK (DESC) : ");
-        list.bubbleSort();
+        // System.out.println("Data mahasiswa setelah sorting berdasarkan IPK (DESC) : ");
+        // list.bubbleSort();
+        // list.tampil();
+
+        System.out.println("Data mahasiswa setelah sorting berdasarkan SELECTION SORT (DESC) : ");
+        list.selectionSort();
         list.tampil();
     }
 }
