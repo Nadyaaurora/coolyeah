74e06764a1bc03d2055368b22fc0df0d7e6c8675
Nadyaaurora <nadyaaurora575@gmail.com>
2025-06-12 23:09:14 +0700

    feat : praktikum 1

---
+++ SMT. 2/ALSD/WEEK 14/source code/Mahasiswa00.java
@@ -0,0 +1,21 @@
+public class Mahasiswa00 {
+    String nim, nama, kelas;
+    double ipk;
+
+    public Mahasiswa00() {}
+
+    public Mahasiswa00(String nim, String nama, String kelas, double ipk) {
+        this.nim = nim;
+        this.nama = nama;
+        this.kelas = kelas;
+        this.ipk = ipk;
+    }
+
+    public void tampilInformasi() {
+        System.out.println("NIM   : " + this.nim + " ");
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK   : " + ipk);
+        System.out.println("-------------------------");
+    }
+}
