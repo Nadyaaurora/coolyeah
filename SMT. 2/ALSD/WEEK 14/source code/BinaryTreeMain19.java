5014f4202f2f179876e148ee59009a045d66f2c8
Nadyaaurora <nadyaaurora575@gmail.com>
2025-06-23 09:39:29 +0700

    feat : update

--- SMT. 2/ALSD/WEEK 14/source code/BinaryTreeMain19.java
+++ SMT. 2/ALSD/WEEK 14/source code/BinaryTreeMain19.java
@@ -15,15 +15,19 @@ public class BinaryTreeMain19 {
         String hasilCari = bst.find(3.54)?"Ditemukan":"Tidak ditemukan";
         System.out.println(hasilCari);
 
+        System.out.print("Cari mahasiswa dengan ipk: 3.22 : ");
+        hasilCari = bst.find(3.22)?"Ditemukan":"Tidak ditemukan";
+        System.out.println(hasilCari);
+
         bst.add(new Mahasiswa19("244160131", "Devi", "A", 3.72));
         bst.add(new Mahasiswa19("244160205", "Ehsan", "D", 3.37));
         bst.add(new Mahasiswa19("244160170", "Fizi", "B", 3.46));
         System.out.println("\nDaftar semua mahasiswa setelah penambahan 3 mahasiswa:");
         System.out.println("InOrder Traversal:");
         bst.traverseInOrder(bst.root);
-        System.out.println("PreOrde Traversal:");
+        System.out.println("\nPreOrder Traversal:");
         bst.traversePreOrder(bst.root);
-        System.out.println("PostOrder Traversal:");
+        System.out.println("\nPostOrder Traversal:");
         bst.traversePostOrder(bst.root);
 
         System.out.println("\nPenghapusan data mahasiswa");
